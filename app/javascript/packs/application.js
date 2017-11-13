15eb514759e58c109f91f7c9ed146013c578d01f
Kazato Sugimoto <uiureo@gmail.com>
2017-11-13 20:54:16 +0900

    Show timestamp at live page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,26 @@
 /* eslint no-console:0 */
 
 const $ = require('jquery')
+const moment = require('moment')
 const POLL_INTERVAL = 10 * 1000
 
 if ($('#live-image').length > 0) {
   const image = $('#live-image')
-  const basePath = image.attr('src')
 
-  setInterval(() => {
-    image.attr('src', `${basePath}?t=${Date.now()}`)
-  }, POLL_INTERVAL)
+  const url = new URL(window.location.href)
+  const basePath = url.pathname
+
+  function update () {
+    image.attr('src', `${basePath}/images/latest?t=${Date.now()}`)
+
+    $.get(`${basePath}/images/latest.json`).then(data => {
+      const timestamp = moment(data.created_at).format('lll')
+      $('.live-image-timestamp').text(timestamp)
+    }, err => {
+      console.error(err)
+    })
+  }
+
+  update()
+  setInterval(update, POLL_INTERVAL)
 }
