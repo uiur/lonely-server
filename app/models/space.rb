e1b4e16f3c06573ac77ebbc989b92f4fb8740c5a
Kazato Sugimoto <uiureo@gmail.com>
2017-11-18 11:28:10 +0900

    Validate space.name

--- app/models/space.rb
+++ app/models/space.rb
@@ -4,6 +4,8 @@ class Space < ApplicationRecord
   has_many :permitted_users, through: :permissions, source: :user
   has_one :space_setting, dependent: :destroy
 
+  validates :name, presence: true, format: { with: /[a-z0-9_-]+/ }, length: { maximum: 20 }
+
   def viewable_by?(user)
     permitted_users.include?(user)
   end
--- test/models/space_test.rb
+++ test/models/space_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class SpaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validation' do
+    assert { Space.create(name: 'foobar').valid? }
+
+    assert { Space.create.valid? == false }
+    assert { Space.create(name: '@@@###////').valid? == false}
+    assert { Space.create(name: 'a' * 1000).valid? == false }
+  end
 end
