aa474a729266e71b4a58a712c8cb805fe4509774
Kazato Sugimoto <uiureo@gmail.com>
2017-11-25 19:53:51 +0900

    Add locale settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
 
   module Error
     class NotFound < StandardError; end
@@ -76,5 +77,11 @@ class ApplicationController < ActionController::Base
   def redirect_to_login
     session[:login_from_path] = request.fullpath
     redirect_to google_oauth2_path
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale]
   end
 end
