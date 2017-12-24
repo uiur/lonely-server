e9a60eb133a4cd44055f505e7837b266a40687af
Kazato Sugimoto <uiureo@gmail.com>
2017-12-24 14:58:04 +0900

    Implement step chooser

--- app/javascript/packs/components/ImagesRoot.tsx
+++ app/javascript/packs/components/ImagesRoot.tsx
@@ -2,11 +2,13 @@ import * as React from 'react'
 import * as moment from 'moment'
 import request from '../lib/request'
 import { RouteComponentProps } from 'react-router'
+import debounce from 'lodash.debounce'
 
 interface State {
   images: any[]
   lastPage: number
   loading: boolean
+  stepInMinute: number
 }
 
 export default class ImagesRoot extends React.Component<RouteComponentProps<any>, State> {
@@ -16,7 +18,8 @@ export default class ImagesRoot extends React.Component<RouteComponentProps<any>
     this.state = {
       images: [],
       lastPage: 0,
-      loading: false
+      loading: false,
+      stepInMinute: 0
     }
   }
 
@@ -24,18 +27,36 @@ export default class ImagesRoot extends React.Component<RouteComponentProps<any>
     this.fetchMore(this.props.match.params.name)
   }
 
-  async fetchMore (id) {
+  reload () {
+    const id = this.props.match.params.name
+
+    this.setState({
+      images: [],
+      lastPage: 0,
+      loading: false
+    }, () => {
+      this.fetchMore(id)
+    })
+  }
+
+  async fetchMore (id: string) {
     if (this.state.loading) {
       return
     }
 
     this.setState({ loading: true })
 
-    const res = await request.get(`/${id}/images`, {
-      params: {
-        page: this.state.lastPage + 1
-      }
-    })
+    const params = {
+      page: this.state.lastPage + 1
+    }
+
+    if (this.state.stepInMinute > 0) {
+      Object.assign(params, {
+        step_in_minute: this.state.stepInMinute
+      })
+    }
+
+    const res = await request.get(`/${id}/images`, { params })
 
     const images = this.state.images.concat(res.data)
 
@@ -46,14 +67,43 @@ export default class ImagesRoot extends React.Component<RouteComponentProps<any>
     })
   }
 
+  stepOnChange (event) {
+    this.setState({
+      stepInMinute: Number(event.target.value)
+    }, () => {
+      this.reloadOnStepChange()
+    })
+  }
+
+  reloadOnStepChange = debounce(() => this.reload(), 1000)
+
   render () {
     return (
       <section>
+        <div className='step-chooser'>
+          <input
+            className='step-slider'
+            type='range'
+            min='0'
+            max='60'
+            step='10'
+            value={this.state.stepInMinute}
+            onChange={this.stepOnChange.bind(this)}
+            />
+
+          <p>
+            {
+              this.state.stepInMinute > 0 &&
+                `every ${this.state.stepInMinute} minutes`
+            }
+          </p>
+        </div>
+
         <div className='row'>
           {
             this.state.images.map(image => {
               return (
-                <div key={image.id} className='col-6'>
+                <div key={image.timestamp} className='col-6'>
                   <a href={image.url}>
                     <img className='grid-image' src={image.url} />
                   </a>
